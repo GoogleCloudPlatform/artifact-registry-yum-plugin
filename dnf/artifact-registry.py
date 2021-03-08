dec653e33385db8cc5ba380e47e796dabbbaeb83
Liam Hopkins <liamh@google.com>
2021-03-08 11:26:32 -0800

    PR feedback

--- dnf/artifact-registry.py
+++ dnf/artifact-registry.py
@@ -1,5 +1,16 @@
-#   manpage for plugin(s)
-#   config file
+#  Copyright 2021 Google LLC
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
 
 import dnf
 
@@ -11,9 +22,9 @@ from google.oauth2 import service_account
 
 
 class ArtifactRegistry(dnf.Plugin):
-  """DNF Plugin for authenticated access to Google Artifact Registry"""
+  '''DNF Plugin for authenticated access to Google Artifact Registry.'''
 
-  name = "artifact-registry"
+  name = 'artifact-registry'
 
   def __init__(self, base, cli):
     super(ArtifactRegistry, self).__init__(base, cli)
@@ -46,7 +57,7 @@ class ArtifactRegistry(dnf.Plugin):
     token = self._get_token()
     if token:
       headers = repo.get_http_headers()
-      new_headers = ("Authorization: Bearer %s" % token,) + headers
+      new_headers = ('Authorization: Bearer %s' % token,) + headers
       repo.set_http_headers(new_headers)
 
   def _get_token(self):
